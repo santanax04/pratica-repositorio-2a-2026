b614574505f2991a4b8cc455d7f5befd84e4e433
Ana Clara Amianti <canaldalara89@gmail.com>
2026-09-09 11:34:36 -0300

    feat: implement UIButton component with variants and styles

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import '@/global.css';
-import { Button, ButtonText } from '@/components/ui/button';
+import { Button, ButtonText } from './components/ui/button';
 
 export default function App() {
   return (
@@ -11,7 +11,7 @@ export default function App() {
       <View style={styles.container}>
         <Text> 2o. Info 2026</Text>
       <Button variant="default" size="default">
-        <ButtonText>Button</ButtonText>
+        <ButtonText>Este é o primeiro botão</ButtonText>
       </Button>
     </View>
     </GluestackUIProvider>
