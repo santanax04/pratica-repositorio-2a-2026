c6da113cbfa3a6fc790a79be7806d002b85153c9
Ana Clara Amianti <canaldalara89@gmail.com>
2026-09-16 12:37:06 -0300

    feat: add Heading component with responsive styles for various heading levels

--- app/home.jsx
+++ app/home.jsx
@@ -1,11 +1,15 @@
 import { Text } from "@/components/ui/text";
 import { Link } from "expo-router";
 import { Box } from "@/components/ui/box";
+import { VStack } from "@/components/ui/vstack";
 
 export default function Home() {
   return (
     <Box className="flex-1 bg-white p-6">
+      <VStack space="lg">
+        
 
+      </VStack>
     </Box>
   );
 }
