d1ca2a80d98850828a156bcf852a830ac66079ed
Ana Clara Amianti <canaldalara89@gmail.com>
2026-09-16 12:09:27 -0300

    fix: update Stack component to include screenOptions for header visibility

--- app/_layout.js
+++ app/_layout.js
@@ -4,7 +4,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
     return (
         <GluestackUIProvider mode="light">
-            <Stack>
+            <Stack screenOptions={{headerShown: false}}>
                 <Stack.Screen name="home" options={{ handleTitle: "Página Principal" }} />
                 <Stack.Screen name="details" options={{ handleTitle: "Detalhes" }} />
             </Stack>
--- app/home.jsx
+++ app/home.jsx
@@ -1,7 +1,7 @@
 import { Text } from "@/components/ui/text";
 import { Link } from "expo-router";
 
-export default function Home() {
+export default function app() {
   return (
     <>  
         <Text>Página Principal</Text>
