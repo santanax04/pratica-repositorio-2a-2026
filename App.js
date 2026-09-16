4fdd421a2dca31fab2b5889f69eb673adc87eac5
Ana Clara Amianti <canaldalara89@gmail.com>
2026-09-16 11:47:14 -0300

    feat: import LinkText component for enhanced link functionality

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import '@/global.css';
 import { Button, ButtonText } from './components/ui/button';
 import { Text } from '@/components/ui/text';
-import { Link } from '@/components/ui/link';
+import { Link, LinkText } from '@/components/ui/link';
 
 export default function App() {
   return (
