b995963e9fbb1d1d65bd0af223ed731cdebcf64f
Ana Clara Amianti <canaldalara89@gmail.com>
2026-09-16 11:39:25 -0300

    feat: add Link component and update App to include navigation

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,11 @@
 import { Text } from "@/components/ui/text";
+import { Link } from "react-router-dom";
 
 export default function App() {
   return (
     <>  
         <Text>Página Principal</Text>
+        <link href="/details">Ir para Detalhes</link>
     </>
   );
 }
