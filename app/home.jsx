d065b28bee904ab135cdc1ec662045a82bcb4eb0
Ana Clara Amianti <canaldalara89@gmail.com>
2026-09-16 12:41:54 -0300

    fix: rename Home component to app and add route description text

--- app/home.jsx
+++ app/home.jsx
@@ -4,11 +4,15 @@ import { Box } from "@/components/ui/box";
 import { VStack } from "@/components/ui/vstack";
 import { Heading } from "@/components/ui/heading";
 
-export default function Home() {
+export default function app() {
   return (
     <Box className="flex-1 bg-white p-6">
       <VStack space="lg">
         <Heading size="xl">Tela Inicial</Heading>
+        <Text size="md">
+              Esta é a rota "/" (arquivo app/home.jsx)
+
+        </Text>
 
       </VStack>
     </Box>
